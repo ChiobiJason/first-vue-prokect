86866a97fa4afac4b924be9fde9e971f1581ed6c
ChiobiJason <chisomchiobi@gmail.com>
2026-01-28 06:51:31 -0700

    Addeed files to repo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1>My First Vue Project!</h1>
+  <h2>Hello, my name is Chisom Chiobi!</h2>
+  <p>
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
+  </p>
+</template>
+
+<style scoped></style>
